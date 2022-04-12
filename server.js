54e963a432ac56066c26d75750fe9bf5c6b9206a
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-04-12 20:48:02 -0300

    Instalação do express; Atualização do server.js; Criação das primeiras rotas no arquivo app.js

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-const http = require('http');
-const port = 3000;
+import app from './src/app.js';
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Rotas de livros',
-    '/autores': 'Rotas de autores',
-    '/editoras': 'Rotas de editoras',
-    '/sobre': 'Rotas de sobre'
-};
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-});
-
-server.listen(port, () => console.log(`Servidor executando em: http://localhost:${port}`))
+app.listen(port, () => console.log(`Servidor executando em: http://localhost:${port}`))

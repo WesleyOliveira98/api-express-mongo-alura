ed3703f58a226f510442ea552404388ca3de1035
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-04-18 20:41:17 -0300

    Associando os livros aos autores cadastrados

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,10 +2,12 @@ import livros from '../models/Livro.js';
 
 class LivroController {
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            if (err) res.status(400).json({erro: err.message});
-            else res.status(200).json(livros);
-        });
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
+                if (err) res.status(400).json({erro: err.message});
+                else res.status(200).json(livros);
+            });
     }
 
     static cadastrarLivro = (req, res) => {
@@ -26,10 +28,12 @@ class LivroController {
 
     static listarLivroPorId = (req, res) => {
         const id = req.params.id;
-        livros.findById(id, (err, livro) => {
-            if (err) res.status(404).json({erro: err.message});
-            else res.status(200).json(livro);
-        });
+        livros.findById(id)
+            .populate('autor', 'nome')
+            .exec((err, livro) => {
+                if (err) res.status(404).json({erro: err.message});
+                else res.status(200).json(livro);
+            });
     }
 
     static excluirLivro = (req, res) => {
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const schema = new mongoose.Schema({
     id: {type: String},
     titulo: {type: String, required: true},
-    autor: {type: String, required: true},
+    autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true},
     editora: {type: String, required: true},
     numeroPaginas: {type: Number}
 });
